0f6d84dc1a0b2257425ddc7fcc701d195ad09f3c
tka4nik <tka4nik@gmail.com>
2024-05-30 18:14:06 +0300

    Finished task 3

---
+++ task-3/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export THRESHOLD=85
+export SMTP_SERVER="smpt.gmail.com"
+export SMTP_PORT=587
+export SMTP_USER="user"
+export SMTP_PASS="password"
+export EMAIL_FROM="example@gmail.com"
+export EMAIL_TO="example2@gmail.com"
